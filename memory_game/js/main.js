972eedfa29d145439cd7ef2d3e4eed4807fa0a1c
cerebrium <nicholas.m.shankland@gmail.com>
2019-06-03 17:39:36 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+
+var cardOne = 'Queen';
+var cardTwo = 'Queen';
+var cardThree = 'King';
+var cardFour = 'King';
+
+console.log('User flipped the ' + cardTwo + '!');
+console.log('User flipped the ' + cardThree + '!');
