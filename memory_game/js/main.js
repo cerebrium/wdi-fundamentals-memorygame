365cc8213f4fda3fb9c94e51271268275e21edea
cerebrium <nicholas.m.shankland@gmail.com>
2019-06-03 20:47:09 -0700

    almost done, have one bug to fix

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,29 @@
 // arrays defining cards and cards in play
+
+
 var cards = [
   {
   rank: 'queen',
   suit: 'hearts',
-  cardImage: "images/queen-of-hearts.png"},
+  cardImage: 'images/queen-of-hearts.png'},
   {
   rank: 'queen',
   suit: 'diamonds',
-  cardImage: "images/queen-of-diamonds.png"},
+  cardImage: 'images/queen-of-diamonds.png'},
   {
   rank: 'king',
   suit: 'hearts',
-  cardImage: "images/king-of-hearts.png"},
+  cardImage: 'images/king-of-hearts.png'},
   {
   rank: 'king',
   suit: 'diamonds',
-  cardImage: "images/king-of-diamonds.png"}
+  cardImage: 'images/king-of-diamonds.png'}
 ];
 var cardsInPlay = [];
 
 // function for checking for matching
-function checkForMatch() {
+function checkForMatch(cardID) {
+    document.getElementById('game-board').setAttribute('src', cards.cardImage);
     if (cardsInPlay[0] === cardsInPlay[1]) {
     console.log("You found a match!");
   } else {
@@ -28,18 +31,26 @@ function checkForMatch() {
   }
 }
 
+function createBoard() {
+  for (var i=0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard(i));
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
 
 // Function for flipping card and logging it
 function flipCard(cardID) {
   cardsInPlay.push(cards[cardID].rank);
-  checkForMatch();
+  checkForMatch(cardID);
   console.log(cards[cardID].rank);
   console.log(cards[cardID].suit);
   console.log(cards[cardID].cardImage);
 }
 
-
-flipCard(2);
+createBoard();
 
 //for (var i=0; i<10; i += 1) {
 //  console.log(i);
