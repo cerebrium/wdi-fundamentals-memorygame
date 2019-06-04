1d07fd3f1cde53b3df824efcdc690d1d6013e714
cerebrium <nicholas.m.shankland@gmail.com>
2019-06-03 18:21:55 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,19 @@
+// arrays defining cards and cards in play
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-var cardOne = 'Queen';
-var cardTwo = 'Queen';
-var cardThree = 'King';
-var cardFour = 'King';
+// variable declaration for card being used
+cardOne = cards[0];
+cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-console.log('User flipped the ' + cardTwo + '!');
-console.log('User flipped the ' + cardThree + '!');
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert('You found a match');
+} else {
+  alert('Those cards don\'t match!');
+}
+console.log(cardsInPlay);
+//for (var i=0; i<10; i += 1) {
+//  console.log(i);
+//}
